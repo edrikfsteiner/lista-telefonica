c7b80725958b860998cde8e3b21969c4e1d37287
edrikfsteiner <edrikfsteiner@gmail.com>
2024-09-24 17:36:37 -0300

    Create Error Interceptor

---
+++ js/interceptors/errorInterceptor.js
@@ -0,0 +1,11 @@
+app.factory('errorInterceptor', function($q) {
+    return {
+        responseError: function(rejection) {
+            if (rejection.status === 404) {
+                $location.path("/error");
+            }
+            
+            return $q.reject(rejection);
+        }
+    }
+});
