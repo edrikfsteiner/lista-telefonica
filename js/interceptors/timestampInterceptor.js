ed7f39cfc8c2dd6bd70f7fd41b0efb6851c9e77a
edrikfsteiner <edrikfsteiner@gmail.com>
2024-09-24 17:36:56 -0300

    Create Timestamp Interceptor

---
+++ js/interceptors/timestampInterceptor.js
@@ -0,0 +1,12 @@
+app.factory('timestampInterceptor', function() {
+    return {
+        request: function(config) {
+            let url = config.url;
+            if (url.indexOf('view') > -1) return config;
+
+            let timestamp = new Date().getTime();
+            config.url = url + "?timestamp=" + timestamp;
+            return config;
+        }
+    };
+});
